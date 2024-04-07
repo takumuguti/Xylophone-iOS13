bbadccbaeb6ec5fd8053511d008a58bd1fb8a356
Takudzwanashe Dylan Muguti <90533439+takumuguti@users.noreply.github.com>
2024-04-08 09:41:47 +1000

    Add to GitHub

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,28 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-
     
-
+    @IBAction func keyPressed(_ sender: UIButton){
+        play((sender.currentTitle)!)
+        
+        sender.alpha = 0.5
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+             sender.alpha = 1.0
+         }
+    }
+    private func play(_ key: String){
+        let url = Bundle.main.url(forResource: key, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+    }
+    
 }
-
